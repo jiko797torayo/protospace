a5116fef1cce2a40ad8a688f7e99c797082dcb88
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-25 09:59:24 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,44 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def new
+    @prototype = Prototype.new
+    @prototype.captured_images.build
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to :root, notice: 'New prototype was successfully created'
+    else
+      redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
+     end
+  end
+
+  def show
+  end
+
+  def destroy
+    @prototype.destroy if @prototype.user_id == current_user.id
+  end
+
+  private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      :user_id,
+      captured_images_attributes: [:content, :status]
+    )
+  end
+end
