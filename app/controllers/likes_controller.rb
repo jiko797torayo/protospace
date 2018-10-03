e8d4b49f0ab0affb5c861399b79e636715417c1a
manami-kk <manami.321go@gmail.com>
2018-10-03 10:57:16 +0900

    fix likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,8 @@
 class LikesController < ApplicationController
-  before_action :set_items, only:[create, destroy]
+  before_action :set_prototype, only:[:create, :destroy]
 
   def create
-    @like = Like.create(user_id: current_user.id, tweet_id: params[:prototype_id])
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
   def destroy
@@ -10,8 +10,7 @@ class LikesController < ApplicationController
     like.destroy
   end
 
-  def set_items
-    @prototypes = Prototype.all
-    @likes = Like.where(prototype_id: params[:prototype_id])
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
