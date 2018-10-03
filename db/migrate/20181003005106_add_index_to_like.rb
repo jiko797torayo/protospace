eae3cdd804a3930d14d6e16b6dc9fa1fa0a19df0
manami-kk <manami.321go@gmail.com>
2018-10-03 10:56:02 +0900

    fix like model

--- db/migrate/20181003005106_add_index_to_like.rb
+++ db/migrate/20181003005106_add_index_to_like.rb
@@ -1,7 +1,7 @@
 class AddIndexToLike < ActiveRecord::Migration
   def change
-    add_index :user_id, :likes
-    add_index :prototype_id, :likes
-    add_index :likes, [:user_id, :prototype_id], unique: true
+    add_index :likes, :user_id
+    add_index :likes, :prototype_id
+    add_index :likes, [:user_id, :prototype_id], :unique => true
   end
 end
